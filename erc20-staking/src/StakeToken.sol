40aadec4d39ac2619d7396bdd3de8dba3865202c
benasphy <binidani1903@gmail.com>
2026-02-21 18:12:52 +0300

    stake token source file

---
+++ erc20-staking/src/StakeToken.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract StakeToken {
+    string public name = "StakeToken";
+    string public symbol = "STK";
+    uint8 public immutable decimals = 18;
+
+    uint256 public totalSupply;
+
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(uint256 initialSupply) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256)
+    {
+        return allowances[owner][spender];
+    }
+
+    function transferFrom(address from, address to, uint256 amount)
+        external
+        returns (bool)
+    {
+        uint256 allowed = allowances[from][msg.sender];
+        require(allowed >= amount, "Not allowed");
+
+        allowances[from][msg.sender] = allowed - amount;
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(balances[from] >= amount, "Insufficient");
+
+        balances[from] -= amount;
+        balances[to] += amount;
+
+        emit Transfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal {
+        totalSupply += amount;
+        balances[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+}
