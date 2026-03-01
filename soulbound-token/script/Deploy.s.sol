0c4930567f288ef292aaac54ed29f5b1fe33eb88
benasphy <binidani1903@gmail.com>
2026-03-01 11:47:11 +0300

    Deploy script file

---
+++ soulbound-token/script/Deploy.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/SoulboundToken.sol";
+
+contract Deploy is Script {
+
+    function run() external {
+
+        vm.startBroadcast();
+
+        SoulboundToken sbt = new SoulboundToken();
+
+        vm.stopBroadcast();
+    }
+}
