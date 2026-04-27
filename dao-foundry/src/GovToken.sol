b42f133111c88e029ceed4e4bc6945fce4c0e62f
benasphy <binidani1903@gmail.com>
2026-04-27 20:04:36 +0300

    source file for GovToken

---
+++ dao-foundry/src/GovToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract GovToken {
+    string public name = "GovToken";
+    string public symbol = "GOV";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    function mint(address to, uint256 amount) external {
+        balanceOf[to] += amount;
+        totalSupply += amount;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balanceOf[msg.sender] >= amount, "no balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
+}
