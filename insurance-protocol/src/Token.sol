7b803bbb6ffb410eacb4337e1793e3a6259d45cf
benasphy <binidani1903@gmail.com>
2026-05-17 12:30:43 +0300

    Token source file

---
+++ insurance-protocol/src/Token.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Token {
+    string public name = "Mock USDC";
+    string public symbol = "mUSDC";
+
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    function mint(address to, uint256 amount) external {
+        balanceOf[to] += amount;
+        totalSupply += amount;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balanceOf[msg.sender] >= amount, "no balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external {
+        require(balanceOf[from] >= amount, "no balance");
+
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+    }
+}
