862cba746cc61935cce79e50615953b38b6ec492
benasphy <binidani1903@gmail.com>
2026-05-25 10:50:48 +0300

    Script file

---
+++ prediction-market/script/Deploy.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+
+import "../src/Token.sol";
+import "../src/PredictionMarket.sol";
+
+contract Deploy is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        Token token = new Token();
+
+        PredictionMarket market =
+            new PredictionMarket(
+                address(token)
+            );
+
+        token.mint(
+            msg.sender,
+            1_000_000 ether
+        );
+
+        vm.stopBroadcast();
+    }
+}
