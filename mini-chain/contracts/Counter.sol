352499669e690cdb18e4ed7c280d5074e94889d0
benasphy <binidani1903@gmail.com>
2026-05-31 11:36:03 +0300

    counter source file

---
+++ mini-chain/contracts/Counter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Counter {
+
+    uint256 public number;
+
+    function increment()
+        external
+    {
+        number++;
+    }
+}
